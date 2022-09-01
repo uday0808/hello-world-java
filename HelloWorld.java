907a81681798fbe71424f1ce5aa2918d10e2bb03
uday0808 <111074941+uday0808@users.noreply.github.com>
2022-09-01 17:51:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("good evening");
+    System.out.println("all of devops family!");
+    System.out.println("Hello world!");
   }
 }
